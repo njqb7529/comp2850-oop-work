4795cd1a823fc764075ba74dffe11a1ac9da0da3
njqb7529 <njqb7529@leeds.ac.uk>
2025-10-28 03:26:07 +0000

    everything

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,26 @@
 // Task 3.1: command line arguments
+import kotlin.system.exitProcess
+
+fun main (args: Array<String>){
+
+    if(args.size < 2)
+    exitProcess(1)
+
+
+    print1(args)
+    print2(args)
+
+}
+
+
+fun print1 (numbers: Array<String>){
+
+    println(numbers[0])
+
+
+}
+
+fun print2 (numbers: Array<String>){
+
+    println(numbers[1])
+}
